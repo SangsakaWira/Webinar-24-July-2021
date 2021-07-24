e1a5a425f76bfb7cb9f178447d5151a685463543
Sangsaka Wira <sangsakawira@gmail.com>
2021-07-24 13:53:39 +0700

    add ppt

---
+++ 1_introduction/example2.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+# python3 example2.py 
+# python example2.py (Windows)
+for i in range(99):
+    print("Hello World!")
+    
+print(10 + 10)
---
+++ 1_introduction/solusi_kalkulator.py
@@ -0,0 +1,6 @@
+panjang = input("Masukkan Panjang: ")
+lebar = input("Masukkan lebar: ")
+
+luas = int(panjang) * int(lebar)
+
+print(luas)
